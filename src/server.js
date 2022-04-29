6efc9deb7fe5f3695c1f14066d6d5bd6ce719c0b
nayoung <iirony@naver.com>
2022-04-29 22:11:57 +0900

    Express 설정하기 (view, server)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import express from "express";
 
 const app = express();
-console.log('qqq');
 
-app.listen(3000);
+// views 설정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+// url/public 경로로 가게되면 public 폴더를 보여주기
+app.use("/public", express.static(__dirname + "/public"));
+
+// views render
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log('qqq');
+app.listen(3000, handleListen);
