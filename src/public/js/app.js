c2ea85fc3086f29a20c464470ecd6ef48de13058
nayoung <iirony@naver.com>
2022-05-17 20:50:10 +0900

    User Video, Audio

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,10 +2,29 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let camera = false;
+
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => {
+            device.kind === "videoinput"
+        });
+
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option)
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 async function getMedia() {
     try{
@@ -15,6 +34,8 @@ async function getMedia() {
         });
 
         myFace.srcObject = myStream;
+
+        await getCameras;
     } catch (e) {
         console.log(e);
     }
@@ -23,6 +44,10 @@ async function getMedia() {
 getMedia();
 
 function handleMuteBtn() {
+    myStream.getAudioTracks().forEach((track) => {
+        track.enabled = !track.enabled;
+    });
+
     if(muted) {
         muteBtn.innerText = "음성 OFF";
         muted = false;
@@ -34,6 +59,10 @@ function handleMuteBtn() {
 }
 
 function handleCameraBtn() {
+    myStream.getVideoTracks().forEach((track) => {
+        track.enabled = !track.enabled;
+    });
+
     if(camera) {
         cameraBtn.innerText = "카메라 OFF";
         camera = false;
